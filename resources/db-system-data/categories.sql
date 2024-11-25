79a636a8b87b90ed1bccb8872655a05baffd8896
mikeyT42 <mrmojorisen0@gmail.com>
2024-11-25 00:17:28 -0500

    Some formatting to get rid of tabs.

--- resources/db-system-data/categories.sql
+++ resources/db-system-data/categories.sql
@@ -1,15 +1,15 @@
 
 INSERT INTO public.categories (description,"name","color",is_user_defined) VALUES
-	 ('No color','None',1,false),
-	 ('Miscellaneous items','Misc.',5,false),
-	 ('Gasoline for Cars','Gas',4,false),
-	 ('Restaurant','Restaurant',2,false),
-	 ('First Mortgage','Mortgage',5,false),
-	 ('Grocery','Grocery',3,false),
-	 ('Cell Phone Bill','Phone',7,false),
-	 ('Internet Bill','Internet',7,false),
-	 ('Electricity Bill','Electric',7,false),
-     ('Payroll','Pay',4,false),
-	 ('Church Tithe','Tithe',6,false),
-     ('Car Insurance','Insurance',3,false),
-     ('Toiletries','Toiletries',4,false);
+    ('No color','None',1,false),
+    ('Miscellaneous items','Misc.',5,false),
+    ('Gasoline for Cars','Gas',4,false),
+    ('Restaurant','Restaurant',2,false),
+    ('First Mortgage','Mortgage',5,false),
+    ('Grocery','Grocery',3,false),
+    ('Cell Phone Bill','Phone',7,false),
+    ('Internet Bill','Internet',7,false),
+    ('Electricity Bill','Electric',7,false),
+    ('Payroll','Pay',4,false),
+    ('Church Tithe','Tithe',6,false),
+    ('Car Insurance','Insurance',3,false),
+    ('Toiletries','Toiletries',4,false);
--- resources/db-system-data/category_colors.sql
+++ resources/db-system-data/category_colors.sql
@@ -1,8 +1,8 @@
 INSERT INTO public.category_colors ("color") VALUES
-	 ('none'::public."color"),
-	 ('red'::public."color"),
-	 ('blue'::public."color"),
-	 ('green'::public."color"),
-	 ('grey'::public."color"),
-	 ('purple'::public."color"),
-	 ('yellow'::public."color");
+    ('none'::public."color"),
+    ('red'::public."color"),
+    ('blue'::public."color"),
+    ('green'::public."color"),
+    ('grey'::public."color"),
+    ('purple'::public."color"),
+    ('yellow'::public."color");
