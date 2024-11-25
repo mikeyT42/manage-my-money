0a84093b53f7562dfdb84658774a2dba0315ed75
Michael Torres <mrmojorisen0@gmail.com>
2024-11-25 00:08:52 -0500

    I am adding some base system data for default data.

---
+++ resources/db-system-data/category_colors.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.category_colors ("color") VALUES
+	 ('none'::public."color"),
+	 ('red'::public."color"),
+	 ('blue'::public."color"),
+	 ('green'::public."color"),
+	 ('grey'::public."color"),
+	 ('purple'::public."color"),
+	 ('yellow'::public."color");
