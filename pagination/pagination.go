c9d31ecdd3dbe7edaf64cb1e439ece9994ac4960
Michael Torres <mrmojorisen0@gmail.com>
2024-11-26 22:42:32 -0500

    I comment to myself.

--- pagination/pagination.go
+++ pagination/pagination.go
@@ -6,6 +6,9 @@ package pagination
 //
 // =============================================================================
 type PaginationState struct {
+	// TODO: I could add an array of integers with the middle element being
+	// guaranteed to be the current page. This could then be used in the middle
+	// of the current and previous page buttons.
 	Previous         int
 	Next             int
 	DisabledPrevious bool
